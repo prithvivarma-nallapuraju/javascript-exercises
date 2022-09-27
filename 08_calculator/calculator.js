3a8908ee8977838193c6c5043c01d228b68ba819
Prithvi Varma Nallapuraju <98509310+prithvivarma-nallapuraju@users.noreply.github.com>
2022-09-27 09:57:24 +0530

    completed the calculator code

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arg) {
+	let sum = 0
+  for(let i=0; i<arg.length;i++) {
+    sum+= arg[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(arg) {
+  let mul = 1
+  for(let i=0;i<arg.length;i++) {
+    mul*=arg[i]
+  }
+  return mul
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let mul = 1
+  if(a == 0 || a == 1) {
+    return 1
+  }else {
+    while(a>0) {
+      mul*=a
+      a--
+    }
+    return mul
+  }
 };
 
 // Do not edit below this line
