836728fdeb9c25f821d29c8d663b761637124674
Prithvi Varma Nallapuraju <98509310+prithvivarma-nallapuraju@users.noreply.github.com>
2022-09-27 21:34:20 +0530

    completed till get the tiles

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let name = ''
+    const d = new Date()
+    for(let i=0;i<people.length;i++) {
+        if(typeof(people[i].yearOfDeath) == 'undefined') {
+            people[i].yearOfDeath = d.getFullYear()
+        }
+        if(i==people.length-1){
+            break
+        }else{
+            const a = people[i].yearOfDeath - people[i].yearOfBirth
+            const b = people[i+1].yearOfDeath - people[i+1].yearOfBirth
+            if(a>b) {
+                name = people[i]
+            }
+        }
+    }
+    return name
 };
 
 // Do not edit below this line
